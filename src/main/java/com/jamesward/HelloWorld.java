eee8285aca335646995042e6b7bc8e04cbac3ba0
James Ward <james@jamesward.org>
2012-09-11 05:51:57 -0600

    init

---
+++ src/main/java/com/jamesward/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.jamesward;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message getMessage() {
+        Message message = new Message();
+        message.value = "hello, world";
+        return message;
+    }
+    
+    static class Message {
+        public String value;
+    }
+
+}
